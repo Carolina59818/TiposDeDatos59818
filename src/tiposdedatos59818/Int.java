a8059d70a00d001743ce51ae3868d5ec79501692
caroc <caroc@192.168.1.71>
2021-03-03 19:48:14 -0600

    Programa que imprime valores mínimos y máximos de cada uno de los tipos de datos primitivos

---
+++ src/tiposdedatos59818/Int.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatos59818;
+
+/**
+ *
+ * @author caroc
+ */
+class Int {
+
+    static int MAX_VALUE;
+    static int MIN_VALUE;
+    
+}
